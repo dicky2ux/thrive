689c737434cad6dd852852b87ef65f7646bfa4e6
Dicky Adhi Satria <dickyadhisatria@upi.edu>
2022-10-29 01:26:12 +0700

    THRIVE

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const router = require("./routes/index.routes");
 const app = express();
 const port = process.env.PORT;
 const morgan = require("morgan")("dev");
-const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger_output.json");
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan);
