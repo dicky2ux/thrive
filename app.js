503928bf0a9db310678835181d9a6c7cd625c27a
Dicky Adhi Satria <dickyadhisatria@upi.edu>
2022-10-29 01:35:40 +0700

    THRIVE

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const express = require("express");
 const router = require("./routes/index.routes");
 const app = express();
 const port = process.env.PORT;
+const cors = require("cors");
 const morgan = require("morgan")("dev");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger_output.json");
 
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://staging-thrive.herokuapp.com/",
+  "https://thrive-project.com/",
+];
+app.use(cors({ origin: whitelist }));
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
